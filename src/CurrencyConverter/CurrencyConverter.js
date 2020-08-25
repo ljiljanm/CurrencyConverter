22c5e72e3bcd0782a51648d1c0f5b36fb42a42a4
Ljiljan Maksimovic <ljiljanm@gmail.com>
2020-08-25 23:38:05 +0200

    Removed 'rate' key from currenices.js, setState better organized, in CurrencyConverter.js removed code from componentDidMount into separate function, which is now being called in componendDidMount

--- src/CurrencyConverter/CurrencyConverter.js
+++ src/CurrencyConverter/CurrencyConverter.js
@@ -6,7 +6,7 @@ import Converter from "./components/Converter";
 
 class CurrencyConverter extends React.Component {
   state = {
-    currencies: currencies,
+    currencies,
     leftCurr: currencies[0],
     rightCurr: currencies[1],
     leftValue: "",
@@ -16,57 +16,46 @@ class CurrencyConverter extends React.Component {
     const code = e.target.value;
     const { currencies } = this.state;
     const ind = currencies.findIndex(item => item.code === code);
-    console.log(ind);
-    this.setState({ rightCurr: currencies[ind] });
     // recalculate the right hand value
-    let rightValue = this.state.leftValue * currencies[ind].rate; // done
-    rightValue = rightValue.toFixed(2);
-    this.setState({ rightValue });
+    let rightValue = (this.state.leftValue * currencies[ind].rate).toFixed(2);
+    this.setState({ rightCurr: currencies[ind], rightValue });
   };
   changeHandler = (e, atr) => {
     const value = e.target.value;
     if (atr === "left") {
-      let rightValue = value * this.state.rightCurr.rate; // done
-      rightValue = rightValue.toFixed(2);
-      this.setState({ leftValue: value });
-      this.setState({ rightValue });
+      let rightValue = (value * this.state.rightCurr.rate).toFixed(2);
+      this.setState({ leftValue: value, rightValue });
     }
     if (atr === "right") {
-      let leftValue = value / this.state.rightCurr.rate; // done
-      leftValue = leftValue.toFixed(2);
-      this.setState({ rightValue: value });
-      this.setState({ leftValue });
+      let leftValue = (value / this.state.rightCurr.rate).toFixed(2);
+      this.setState({ rightValue: value, leftValue });
     }
   };
-  componentDidMount() {
+
+  fetchData = async () => {
     const { currencies } = this.state;
     let rates = {};
-    const url =
-      "http://data.fixer.io/api/latest?access_key=443ef2f31753d239068100ede5ebbbbb&symbols=USD,AUD,CAD,JPY,GBP,BAM,RSD";
-    fetch(url)
-      .then(response => {
-        console.log(response);
-        return response.json();
-      })
-      .then(data => (rates = data.rates))
-      .then(() => {
-        localStorage.setItem("rates", JSON.stringify(rates));
-        for (let key in rates) {
-          const ind = currencies.findIndex(item => item.code === key);
-          currencies[ind].rate = rates[key];
-        }
-        this.setState({ currencies });
-      })
-      .catch(error => {
-        console.log("there s an error");
-        rates = JSON.parse(localStorage.getItem("rates"));
-        console.log(rates);
-        for (let key in rates) {
-          const ind = currencies.findIndex(item => item.code === key);
-          currencies[ind].rate = rates[key];
-        }
-        this.setState({ currencies });
-      });
+    try {
+      const url =
+        "http://data.fixer.io/api/latest?access_key=443ef2f31753d239068100ede5ebbbbb&symbols=USD,AUD,CAD,JPY,GBP,BAM,RSD";
+      const response = await fetch(url);
+      const data = await response.json();
+      rates = data.rates;
+    } catch (error) {
+      console.error("there's an error", error);
+      rates = JSON.parse(localStorage.getItem("rates"));
+    }
+
+    localStorage.setItem("rates", JSON.stringify(rates));
+    for (let key in rates) {
+      const ind = currencies.findIndex(item => item.code === key);
+      currencies[ind].rate = rates[key];
+    }
+    this.setState({ currencies });
+  };
+
+  async componentDidMount() {
+    this.fetchData();
   }
   render() {
     return (
@@ -83,10 +72,10 @@ class CurrencyConverter extends React.Component {
             leftValue={this.state.leftValue}
             rightValue={this.state.rightValue}
           />
-
           <div className="rate">
             <strong>Exchange rate: </strong>1 {this.state.leftCurr.code} ={" "}
-            {this.state.rightCurr.rate.toFixed(4)} {this.state.rightCurr.code}{" "}
+            {this.state.rightCurr.rate ? this.state.rightCurr.rate.toFixed(4) : null}{" "}
+            {this.state.rightCurr.code}{" "}
           </div>
           <div className="signature">By Ljiljan Maksimovic</div>
         </div>
--- src/CurrencyConverter/components/currencies.js
+++ src/CurrencyConverter/components/currencies.js
@@ -1,50 +1,34 @@
 export const currencies = [
   {
-    id: 1,
     name: "Euro",
     code: "EUR",
-    rate: 1, // these are only TEMPORARY rates which will be updated from
-  }, // https://fixer.io/ or from localStorage if netWork access not available
+  },
   {
-    id: 2,
     name: "American Dollar",
     code: "USD",
-    rate: 1,
   },
   {
-    id: 3,
     name: "Canadian Dollar",
     code: "CAD",
-    rate: 1,
   },
   {
-    id: 4,
     name: "Australian Dollar",
     code: "AUD",
-    rate: 1,
   },
   {
-    id: 5,
     name: "Japanese Yen",
     code: "JPY",
-    rate: 1,
   },
   {
-    id: 6,
     name: "Brittish Pound",
     code: "GBP",
-    rate: 1,
   },
   {
-    id: 7,
     name: "Bosnian KM",
     code: "BAM",
-    rate: 1,
   },
   {
-    id: 8,
     name: "Serbian Dinar",
     code: "RSD",
-    rate: 1,
   },
 ];
